1448b147edcfd869bc7a3b09e815bc84621da5b9
Tejas-Billava <tejas.billava5@gmail.com>
2024-10-20 05:59:31 +0000

    app chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
-
+import Footer from"./components/Footer/Footer"
+import Navbar from './components/Navbar'
+import Testimonials from './components/Testimonials'
 function App() {
 
   return (
     <div className=''>
+      <Navbar></Navbar>
+      <div className='max-w-7xl mx-auto pt-20 px-6'> 
+      <Testimonials></Testimonials>
       
+      </div>
+      <Footer></Footer>
     </div>
   )
 }
